6f96deb8c0501fed0b2fea6c6b40106183e45c61
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-04-16 00:07:32 +0300

    feat: solved transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("\'arr\' parameter must be an instance of the Array!");
+  } else {
+  let res = arr.slice();
+      for (let i = 0; i < res.length; i++) {
+      if (res[i] === '--discard-prev') {
+          if (i !== 0) {
+              res[i - 1] = "***"
+              res[i] = res[i - 1];
+          } else {
+              res[i] = "***";
+          }
+      }
+      for (let i = 0; i < res.length; i++) {
+        if (res[i] === '--discard-next') {
+            if (i !== res.length - 1) {
+                res[i + 1] = "***"
+                res[i] = res[i + 1];
+            } else {
+                res[i] = "***";
+            }
+        }
+    }
+      for (let i = 0; i < res.length; i++) {
+          if (res[i] === '--double-prev') {
+              if (i !== 0) {
+                  res[i] = res[i - 1];
+              } else {
+                  res[i] = "***";
+              }
+          }
+      }
+      for (let i = 0; i < res.length; i++) {
+          if (res[i] === '--double-next') {
+              if (i !== res.length - 1) {
+                  res[i] = res[i + 1];
+              } else {
+                  res[i] = "***";
+              }
+          }
+      }
+      }
+      console.log(res.filter((item) => item !== "***"));
+      return res.filter((item) => item !== "***");
+}
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -2,18 +2,34 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ function getSeason(date) {
+  console.log(date)
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  } else if (!(typeof date === 'object') || !Date.parse(date) || !(date instanceof Date)) {
+  console.log('Invalid date!')
+    return 'Invalid date!';
+  } else {
+  let month = date.getMonth() + 1;
+  if (month >= 3 && month <= 5) {
+    return 'spring';
+  } else if (month >= 6 && month <= 8) {
+    return 'summer';
+  } else if (month >= 9 && month <= 11) {
+    return 'fall';
+  } else {
+    return 'winter';
+  }}
 }
 
 module.exports = {
